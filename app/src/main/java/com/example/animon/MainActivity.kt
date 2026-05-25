7db125c1268f3f3fe4a1b5b8ea94f120abaa4b6f
s3rdi <pserdeczna@proton.me>
2026-05-25 20:42:21 +0200

    Initial project setup with compose skeleton

---
+++ app/src/main/java/com/example/animon/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.animon
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.animon.core.designsystem.ui.theme.AnimonTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AnimonTheme {
+            }
+        }
+    }
+}
